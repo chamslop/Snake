49f050e37115bb5b22de58483b2640189655983e
sHesl <sHesl@users.noreply.github.com>
2018-06-23 19:08:04 +0100

    Head, middle and tail

--- Assets/Scripts/Controllers/SnakeController.cs
+++ Assets/Scripts/Controllers/SnakeController.cs
@@ -8,6 +8,7 @@ public class SnakeController : MonoBehaviour {
 	// Components
 	private Animator anim;
 	private Rigidbody2D rb2d;
+	private GameObject baseMiddle;
 	private GameObject tail;
 
 	// Internals
@@ -17,21 +18,27 @@ public class SnakeController : MonoBehaviour {
 	void Start() {
 		anim = GetComponent<Animator>();
 		rb2d = GetComponent<Rigidbody2D>();
+		
+		baseMiddle = GameObject.Find("Middle");
 		tail = GameObject.Find("Tail");
 
 		EventManager.OnPointScored += speedUp;
 
-		rb2d.AddForce(new Vector2(0, 1 * defaultSpeed * Time.fixedDeltaTime));
+		rb2d.AddForce(new Vector2(0, -1 * defaultSpeed * Time.fixedDeltaTime));
 	}
 	
 	void FixedUpdate() {
 		Vector3 headPreviousPosition =  this.transform.position;
+		Vector3 middlePreviousPosition =  baseMiddle.transform.position;
 
 		applyMovementForce();
 
 		Vector3 headCurrentPosition =  this.transform.position;
-		followPreviousSegment(headPreviousPosition, headCurrentPosition, tail);
-		
+		Vector3 middleCurrentPosition =  baseMiddle.transform.position;
+		followPreviousSegment(headPreviousPosition, headCurrentPosition, baseMiddle);
+		followPreviousSegment(middlePreviousPosition, middleCurrentPosition, tail);
+
+		// Inform animator of snake direction to trigger appropriate wiggle
 		anim.SetFloat("SnakeVelocityX", rb2d.velocity.x);
 		anim.SetFloat("SnakeVelocityY", rb2d.velocity.y);
 	}
@@ -43,31 +50,40 @@ public class SnakeController : MonoBehaviour {
 	}
 
 	private void applyMovementForce() {
+		Vector2 force = Vector2.zero;
+		int rotation = 0;
+
 		if (isInputDown() && rb2d.IsTravellingHorizontally()) {
-			rb2d.velocity = Vector2.zero;
-			rb2d.AddForce(new Vector2(0, -1 * speed * Time.fixedDeltaTime));
-			return;
+			force = new Vector2(0, -1 * speed * Time.fixedDeltaTime);	
+			rotation = 0;
 		} else if (isInputUp() && rb2d.IsTravellingHorizontally()) {
-			rb2d.velocity = Vector2.zero;
-			rb2d.AddForce(new Vector2(0, 1 * speed * Time.fixedDeltaTime));
-			return;
+			force = new Vector2(0, 1 * speed * Time.fixedDeltaTime);
+			rotation = 180;
 		} else if (isInputLeft() && rb2d.IsTravellingVertically()) {
-			rb2d.velocity = Vector2.zero;
-			rb2d.AddForce(new Vector2(-1 * speed * Time.fixedDeltaTime, 0));
-			return;
+			force = new Vector2(-1 * speed * Time.fixedDeltaTime, 0);
+			rotation = 270;
 		} else if (isInputRight() && rb2d.IsTravellingVertically()) {
-			rb2d.velocity = Vector2.zero;
-			rb2d.AddForce(new Vector2(1 * speed * Time.fixedDeltaTime, 0));
+			force = new Vector2(1 * speed * Time.fixedDeltaTime, 0);
+			rotation = 90;
 		}
+
+		// If no input was entered this frame, the previously applied force will continue
+		// to operate unimpeded, as we have disabled drag/gravity.
+		if (force == Vector2.zero) return; 
+
+		rb2d.velocity = Vector2.zero;
+		rb2d.AddForce(force);
+		rb2d.MoveRotation(rotation);
 	}
 
 	private IEnumerator waitAndTriggerRespawn() {
 		//hide
 		yield return new WaitForSeconds(0.3f);
 		//show
-		rb2d.transform.position = Vector3.zero;
-		speed = defaultSpeed;
-		rb2d.AddForce(new Vector2(0, 1 * speed));
+		rb2d.transform.position = Vector3.zero; // Reset position to the center of the game
+		rb2d.MoveRotation(0); // Reset head to face down
+		speed = defaultSpeed;	
+		rb2d.AddForce(new Vector2(0, -1 * speed * Time.fixedDeltaTime));
 		yield return null;
 	}
 
